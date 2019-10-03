f2ead8273198e596ae71d2dca1227f89495040a6
ryan.snyder <ryan.snyder@screenscape.com>
2019-10-03 14:03:45 -0300

    Added basic router and example

--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import client from '../../feather/feathers'
-import logo from './logo.svg';
-import Login from '../../components/login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
-import Paper from '@material-ui/core/Paper';
 import './App.css';
 
 
 // Use lazy loading here
+const Login = lazy(() => import('../../components/login'));
+// TODO: Move this into pages
+// And then lazy load them as well
+function Character() {
+  return <li> Character Creation/Viewer</li>
+}
+
+function Party() {
+  return <li> Party Creation/Viewer</li>
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -40,27 +49,42 @@ class App extends Component {
       message: 'User connected'
     });
   }
+
   render() {
-    console.log('State was updated?');
     const { signedIn, user } = this.state;
-    console.log(signedIn);
+    /**
+     * TODO:
+     * Add a Router Below the app bar
+     * Add Character creation
+     * Move AppBar and Login into a NavBar component
+     * 
+     */
     return (
+      <Router>
       <div className="App">
-        <AppBar color="default" position="static">
-          <Login signedIn={signedIn} user ={user} />
-        </AppBar>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This will redirect the user to the appropriate page:
-          </p>
-          <ul>
-            <li> We need to implement router here</li>
-            <li> As well we should make a sign up page</li>
-            <li> Character Creation/Viewer</li>
-            <li> Party Creation/Viewer</li>
-            <li> Signup</li>   
-          </ul> 
+        <Suspense fallback={<div> Loading... </div>}>
+          <AppBar color="default" position="static">
+            <Login signedIn={signedIn} user ={user} />
+          </AppBar>
+            <p>
+              This will redirect the user to the appropriate page:
+            </p>
+            <ul>
+              <Switch>
+                <Route path="/party">
+                  <Party />
+                </Route>
+                <Route path="/character">
+                  <Character />
+                </Route>
+                <Route path="/">
+                  <Character />
+                </Route>
+              </Switch>
+            </ul> 
+          </Suspense>
       </div>
+      </Router>
     );
   }
 }
