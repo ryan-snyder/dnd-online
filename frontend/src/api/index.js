f38a6217588609606ee7f4b97440b21e760d0f1e
DESKTOP-HSLA09I\Ryan <spocksbeard16@gmail.com>
2019-10-05 19:24:24 -0300

    Started on character creation. API we're using is dumb so I'm going to do it myself

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import superagent from 'superagent';
-
+import superagentCache from 'superagent-cache';
 /**
  * Index file for api folder
  * 
@@ -10,5 +10,20 @@ import superagent from 'superagent';
  * 
  * 
  * 
+ * TODO:
+ * Download this api myself because right now you can't make browser requests with it which is dumb
+ * 
+ * 
+ * or we could serve it myself but I'd rather not
  * 
  */
+superagentCache(superagent);
+
+const baseUrl = 'http://dnd5eapi.co/api/';
+
+
+export const getClasses = () => superagent.get(`${baseUrl}classes`);
+
+export const getSpells = (query = {}) => superagent.get(`${baseUrl}spells`).query(query);
+
+export const getFeatures = (query = {}) => superagent.get(`${baseUrl}features`).query(query);
--- frontend/src/pages/CharacterCreation.js
+++ frontend/src/pages/CharacterCreation.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import { getClasses } from '../api';
 
 
 
 function CharacterCreation(props) {
     const { signedIn, user } = props;
+    const [ charClass, setClass] = React.useState({
+        charClass: ''
+    });
+
+    const [ options, setOptions ] = React.useState([]);
+
+    const handleChange = (event) => {
+        setClass(event.target.value);
+    }
+
+    React.useEffect(() => {
+        getClasses().then(res => {
+            console.log(res.results);
+            setOptions(res.results)
+        });
+    })
 
     return (
         <span>
             <p>Character Creation Screen</p>
             {signedIn ? <p>Welcome {user.email}</p> : <p> You are not logged in but you can still make a character</p>}
-
+            <Select
+                value={charClass}
+                onChange={handleChange}
+            >
+                {options.forEach(option => 
+                        <MenuItem value={option.name}>{option.name}</MenuItem>
+                )}
+            </Select>
         </span>
     )
 }
