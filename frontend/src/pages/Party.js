c10256aabe744c238da6be0096de52c9c0128d3a
Ryan <spocksbeard16@gmail.com>
2020-12-09 11:56:19 -0400

    fix party.js

--- frontend/src/pages/Party.js
+++ frontend/src/pages/Party.js
@@ -12,7 +12,7 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import client from '../feather/feathers';
+import client from '../feather/client';
 import { Context } from '../Store/Store';
 
 
